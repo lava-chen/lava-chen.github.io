aaf559285bc694147a4453bdff011f87dcf6fdf0
Xuanyu Chen <150888349+lava-chen@users.noreply.github.com>
2026-04-28 19:29:51 +0800

    remove notes link from nav

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: "/", title: "Home" },
   { href: "/blog", title: "Blog" },
-  { href: "https://notes.lavachen.org", title: "Notes" },
+
   { href: "/projects", title: "Projects" },
   { href: "/about", title: "About" },
   { href: "/cv", title: "CV" },
